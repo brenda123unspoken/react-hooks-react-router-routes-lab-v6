de8f6caef6aac911dfa461291ae8e5d877c72af8
brenda123unspoken <brendamukaya@gmail.com>
2024-06-15 10:29:03 +0300

    intial commit

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,30 @@
+// Import React and necessary components
+import React from 'react';
+import Home from './pages/Home';
+import Directors from './pages/Directors';
+import Actors from './pages/Actors';
+import Movie from './pages/Movie';
+import ErrorPage from './pages/Error.Page';
 
-
+// Define your routes
 const routes = [
-   
-  ];
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/directors',
+    element: <Directors />,
+  },
+  {
+    path: '/actors',
+    element: <Actors />,
+  },
+  {
+    path: '/movie/:id',
+    element: <Movie />,
+  },
+];
 
 export default routes;
